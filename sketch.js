cd8f410331c41b982a0958f91854b95f7ad38f3d
WXander <72658646+WXander@users.noreply.github.com>
2021-06-10 21:32:40 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var bullet, wall;
+var speed, weight;
+var thickness;
+
+
+
+function setup() {
+  createCanvas(1600,400);
+
+  bullet = createSprite(50,200,100,20);
+  wall = createSprite(1200,200,thickness,height/2);
+  wall.shapeColor = (80,80,80);
+  
+
+  thickness=random(22,83);
+  weigth=random(30,52);
+  speed=random(223,321);
+  bullet.velocityX = speed;
+}
+
+function draw() {
+  background(0);  
+
+  
+
+if (wall.x-bullet.x < (bullet.width+wall.width)/2){
+  bullet.velocityX = 0;
+  var damage=(0.5 * weight * speed * speed)/(thickness*thickness*thickness);
+  if(damage>10){
+    bullet.shapeColor = "red";
+  }
+  else{
+    bullet.shapeColor = "green";
+  }
+}
+
+console.log(damage)
+  drawSprites();
+}
